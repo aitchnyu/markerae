630cd614a06e188e0c0aa9e2527316fd5c95f945
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:15 +0530

    Dumb command

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
 docker build -t markerae .
 # todo run proxy here
 # here local postgres host
-docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="172.17.0.1" -it markerae /bin/bash
+docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="172.17.0.1" -it markerae ls -la
 docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="172.17.0.1" -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
 #docker run --mount type=bind,source="$(pwd)",target=/code -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
