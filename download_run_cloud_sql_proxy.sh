77e655017e8001b8f35d68d81d0e2ca087ed8bb9
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:16 +0530

    Script to create bucket and sql db 2

--- download_run_cloud_sql_proxy.sh
+++
@@ -1,5 +0,0 @@
-apt install wget
-wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
-# todo not in tmp
-chmod +x cloud_sql_proxy
-nohup cloud_sql_proxy -instances=marker-311206:asia-southeast1:pg=tcp:0.0.0.0:5432 &
