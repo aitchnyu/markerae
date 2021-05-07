8db2e5ac582d14d125bf2f107dbb26db023acdfa
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:15 +0530

    Manage, deploy commands in script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-# todo copy appenv.yaml from sample file
-echo "  POSTGRES_DB: ${POSTGRES_DB}" >> backend/appenv.yaml
-echo "  POSTGRES_USER: ${POSTGRES_USER}" >> backend/appenv.yaml
-echo "  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}" >> backend/appenv.yaml
-echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
-#/code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
 docker build -t markerae .
-# todo run proxy here
-# here local postgres host
-#docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae ls -la
-docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
-#docker run --mount type=bind,source="$(pwd)",target=/code -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
+
+if [[ -z "$POSTGRES_HOST" ]]
+then
+  echo looks like you didnt set any env variables
+  exit 1
+fi
+
+if [[ $1 == "manage" ]]
+then
+  shift
+  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py "$@"
+elif [[ $1 == "deploy" ]]
+then
+  echo "env_variables:" > backend/appenv.yaml
+  echo "  POSTGRES_DB: ${POSTGRES_DB}" >> backend/appenv.yaml
+  echo "  POSTGRES_USER: ${POSTGRES_USER}" >> backend/appenv.yaml
+  echo "  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}" >> backend/appenv.yaml
+  echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
+  /code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
+  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py migrate
+  gcloud app deploy
+else
+  echo unknown command
+fi
