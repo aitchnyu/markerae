5c89bae83a7d992e60aae1ccca9f07987465b97b
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:16 +0530

    Deploy command for new run

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 if [[ -z "$PROJECT_ID" ]]
 then
-  echo looks like you are not running this in GCloud Shell
+  echo It appears you are not running this in GCloud Shell.
   exit 1
 fi
 if [[ -z "$POSTGRES_HOST" ]]
 then
-  echo looks like you didnt set any env variables
+  echo It appears you didnt set necessary env variables
   exit 1
 fi
 
@@ -19,13 +19,41 @@ then
   docker run --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it "gcr.io/${PROJECT_ID}/my-image" python3 manage.py "$@"
 elif [[ $1 == "deploy" ]]
 then
-   docker run --mount type=bind,source="$(pwd)",target=/hostpwd -it "gcr.io/${PROJECT_ID}/my-image" bash -c "python3 manage.py collectstatic --noinput && cp -R static /hostpwd/static"
+   docker run --mount type=bind,source="$(pwd)",target=/hostpwd \
+      -it "gcr.io/${PROJECT_ID}/my-image" \
+      bash -c "python3 manage.py collectstatic --noinput && cp -R static /hostpwd/static"
    gsutil -m rm gs://${STATIC_BUCKET}/**
    gsutil -m cp -Z -r static/** "gs://${STATIC_BUCKET}/"
    sudo rm -r static
    docker push "gcr.io/${PROJECT_ID}/my-image"
+   gcloud_deploy () {
+      gcloud run deploy "${SERVICE_NAME}" \
+        --image "gcr.io/${PROJECT_ID}/my-image:latest" \
+        --platform managed \
+        --region "${REGION}" \
+        --max-instances 5 \
+        --cpu 1 \
+        --memory 256Mi \
+        --timeout 10 \
+        --concurrency 10 \
+        --ingress all \
+        --allow-unauthenticated \
+        --set-cloudsql-instances "${POSTGRES_HOST}"\
+        --update-env-vars POSTGRES_DB="${POSTGRES_DB}" \
+        --update-env-vars POSTGRES_USER="${POSTGRES_USER}" \
+        --update-env-vars POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
+        --update-env-vars POSTGRES_HOST="${POSTGRES_HOST}" \
+        --update-env-vars ALLOWED_HOSTS="$1" \
+        --update-env-vars STATIC_URL="https://storage.googleapis.com/${STATIC_BUCKET}/"
+   }
+   gcloud_deploy fake_host
+   while true
+   do
+     gcloud run services describe "${SERVICE_NAME}" --platform managed --region "${REGION}" --format json | jq '.status.address.urls'
+     sleep 5
+   done
+
    # todo push to cloud run with env variables
-   gcloud run deploy dd --image "gcr.io/${PROJECT_ID}/my-image:latest" --platform managed --region "${REGION}" --max-instances=5
 else
   echo unknown command
 fi
