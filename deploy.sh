8da31c46da8c00e02729d26791801d9ee8b50f48
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:15 +0530

    Bind to unix socket

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# todo copy appenv.yaml from sample file
 echo "  POSTGRES_DB: ${POSTGRES_DB}" >> backend/appenv.yaml
 echo "  POSTGRES_USER: ${POSTGRES_USER}" >> backend/appenv.yaml
 echo "  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}" >> backend/appenv.yaml
@@ -7,6 +8,6 @@ echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
 docker build -t markerae .
 # todo run proxy here
 # here local postgres host
-docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="172.17.0.1" -it markerae ls -la
-docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="172.17.0.1" -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
+#docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae ls -la
+docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
 #docker run --mount type=bind,source="$(pwd)",target=/code -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
