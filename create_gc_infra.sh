250286b5f36cc8a619d477aee710e93d8f575ca3
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:16 +0530

    Script to create db 4

--- create_gc_infra.sh
+++ create_gc_infra.sh
@@ -4,7 +4,6 @@ then
   gsutil defacl set public-read "gs://${2}"
 elif [[ $1 == "db" ]]
 then
-  echo Not implemented
   gcloud sql instances create $2 \
     `# Not HA` \
     --availability-type zonal \
@@ -15,6 +14,14 @@ then
     --storage-size 10 \
     --storage-type SSD \
     --tier db-f1-micro
+elif [[ $1 == "test" ]]
+then
+  sudo mkdir -p/cloudsql && sudo chown $USER:$USER /cloudsql
+  CONNECTION_NAME=$(gcloud sql instances describe test --format json | jq -r '.connectionName')
+  nohup ~/cloud_sql_proxy -instances="${CONNECTION_NAME}" -dir=/cloudsql &
+  PROXY_PID=$!
+  PGAPSSWORD=testpasswd psql -h 127.0.0.1 psql -d postgres -u postgres -c 'create extension postgis;'
+  kill "${PROXY_PID}"
 else
   echo Command to create GCS bucket and Cloud sql db.
   echo ./create_gc_infra.sh bucket [your bucket name]
