481133e8522f66d64229ab052b6a2498fda091bd
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:16 +0530

    Script to ensure cloud sql proxy 3

--- ensure_gc_sql_proxy.sh
+++ ensure_gc_sql_proxy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ -f ~/cloud_sql_proxy ]]; then
-    echo "$FILE exists."
+if [[ ! -f ~/cloud_sql_proxy ]]; then
+    echo Downloading google cloud sql proxy
     wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O ~/cloud_sql_proxy
     chmod +x ~/cloud_sql_proxy
 fi
