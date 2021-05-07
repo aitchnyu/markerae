115e701bd569b02513014c397c7e7b3e793bcee8
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:15 +0530

    Trying to run proxy in background 3

--- download_run_cloud_sql_proxy.sh
+++ download_run_cloud_sql_proxy.sh
@@ -1,4 +1,5 @@
 apt install wget
-wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O /tmp/cloud_sql_proxy
-chmod +x /tmp/cloud_sql_proxy
-nohup /tmp/cloud_sql_proxy -instances=marker-311206:asia-southeast1:pg=tcp:0.0.0.0:5432 &
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+# todo not in tmp
+chmod +x cloud_sql_proxy
+nohup cloud_sql_proxy -instances=marker-311206:asia-southeast1:pg=tcp:0.0.0.0:5432 &
