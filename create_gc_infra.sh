823a7510d477fabe33130e0a16d2fb68ddd0bcff
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:16 +0530

    Script to create db 9

--- create_gc_infra.sh
+++ create_gc_infra.sh
@@ -22,7 +22,7 @@ then
   PROXY_PID=$!
   echo $PROXY_PID
   ps ax | grep proxy
-  PGAPSSWORD=testpasswd psql -h $CONNECTION_NAME -d postgres -U postgres -c 'create extension postgis;'
+  PGPASSWORD=testpasswd psql -h "/cloudsql/$CONNECTION_NAME" -d postgres -U postgres -c 'create extension postgis;'
   kill "${PROXY_PID}"
 else
   echo Command to create GCS bucket and Cloud sql db.
