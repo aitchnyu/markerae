09ae65f5a2936c4df022b875a12574a52a81e906
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:16 +0530

    Script to create db

--- create_gc_infra.sh
+++ create_gc_infra.sh
@@ -5,8 +5,17 @@ then
 elif [[ $1 == "db" ]]
 then
   echo Not implemented
+  gcloud sql instances create $2 \
+    `# Not HA` \
+    --availability-type zonal \
+    --database-version POSTGRES_13 \
+    --root-password $3 \
+    --storage-auto-increase \
+    -storage-size 10 \
+    --storage-type SSD \
+    -- tier db-f1-micro
 else
   echo Command to create GCS bucket and Cloud sql db.
   echo ./create_gc_infra.sh bucket [your bucket name]
-  echo ./create_gc_infra.sh db [your db name]
+  echo ./create_gc_infra.sh db [instance name] [root password]
 fi
