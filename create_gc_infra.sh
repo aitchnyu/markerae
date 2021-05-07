046bb5767c9dc58e90b13eb4b8e433c790954c98
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:16 +0530

    Script to create db 14

--- create_gc_infra.sh
+++ create_gc_infra.sh
@@ -19,13 +19,13 @@ then
   sudo mkdir -p /cloudsql && sudo chown $USER:$USER /cloudsql
   CONNECTION_NAME=$(gcloud sql instances describe test --format json | jq -r '.connectionName')
   nohup ~/cloud_sql_proxy -instances="${CONNECTION_NAME}" -dir=/cloudsql &
-  sleep 5
+  sleep 3
   PROXY_PID=$!
   echo $PROXY_PID
   echo $CONNECTION_NAME
   ps ax | grep proxy
-  psql -h "/cloudsql/marker-311206:us-central1:test"
-  psql -h "/cloudsql/$CONNECTION_NAME"
+#  psql -h "/cloudsql/marker-311206:us-central1:test"
+#  psql -h "/cloudsql/$CONNECTION_NAME"
   PGPASSWORD=testpasswd psql -h "/cloudsql/$CONNECTION_NAME" -d postgres -U postgres -c 'create extension postgis;'
   read
   kill "${PROXY_PID}"
