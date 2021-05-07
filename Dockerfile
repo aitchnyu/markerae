3ba607512c5ec124b7f5ae1727e9555e70447bb9
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:16 +0530

    Trying 18.04, removed redundant docker image tag, added push command

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.04 as base
+FROM ubuntu:18.04 as base
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
 WORKDIR /code
--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-docker build --target prod --tag markeraeprod --tag "gcr.io/${PROJECT_ID}/my-image" .
 
+if [[ -z "$PROJECT_ID" ]]
+then
+  echo looks like you are not running this in GCloud Shell
+  exit 1
+fi
 if [[ -z "$POSTGRES_HOST" ]]
 then
   echo looks like you didnt set any env variables
   exit 1
 fi
 
+docker build --target prod --tag "gcr.io/${PROJECT_ID}/my-image" .
+
 if [[ $1 == "manage" ]]
 then
   shift
-  docker run --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markeraeprod python3 manage.py "$@"
+  docker run --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it "gcr.io/${PROJECT_ID}/my-image" python3 manage.py "$@"
 elif [[ $1 == "deploy" ]]
 then
-   docker run --mount type=bind,source="$(pwd)",target=/hostpwd -it markeraeprod bash -c "python3 manage.py collectstatic --noinput && cp -R static /hostpwd/static"
+   docker run --mount type=bind,source="$(pwd)",target=/hostpwd -it "gcr.io/${PROJECT_ID}/my-image" bash -c "python3 manage.py collectstatic --noinput && cp -R static /hostpwd/static"
+   docker push "gcr.io/${PROJECT_ID}/my-image"
    # Hack: static files have root permissions
 #   docker run --mount type=bind,source="$(pwd)",target=/hostpwd -it markeraeprod bash -c "rm -r /hostpwd/static"
   # todo build container, upload static files, push to cloud run with env variables
-#  echo "env_variables:" > backend/appenv.yaml
-#  echo "  POSTGRES_DB: ${POSTGRES_DB}" >> backend/appenv.yaml
-#  echo "  POSTGRES_USER: ${POSTGRES_USER}" >> backend/appenv.yaml
-#  echo "  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}" >> backend/appenv.yaml
-#  echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
-#  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
-#  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py migrate
-#  gcloud app deploy backend/app.yaml
 else
   echo unknown command
 fi
