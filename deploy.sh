9d0f9ef2a1fcb7ec86bdcba7ec3fcc50937f73fe
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:15 +0530

    Pass env variables to Docker

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ echo "  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}" >> backend/appenv.yaml
 echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
 #/code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
 docker build -t markerae .
-docker run --mount type=bind,source="$(pwd)",target=/code -it markerae /bin/bash
-docker run --mount type=bind,source="$(pwd)",target=/code -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
+docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /bin/bash
+docker run --mount type=bind,source="$(pwd)",target=/code -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
+#docker run --mount type=bind,source="$(pwd)",target=/code -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
