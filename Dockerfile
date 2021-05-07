4653ee47550a6ad8d19018e48e7195af352d93f8
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:15 +0530

    Trying multi stage

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.04
+FROM ubuntu:20.04 as base
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
 WORKDIR /code
@@ -6,10 +6,27 @@ WORKDIR /code
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && \
     apt-get install -y python3 python3-pip python3-venv libpq-dev python3-dev && \
-    apt-get install -y binutils libproj-dev gdal-bin && \
-    apt-get install -y curl && \
-    curl -sL https://deb.nodesource.com/setup_14.x | bash - && \
-    apt-get install -y nodejs
+    apt-get install -y binutils libproj-dev gdal-bin
+
+FROM base as dev
 ADD ./bash_start.sh /tmp/bash_start.sh
 RUN cat /tmp/bash_start.sh >> ~/.bashrc
 EXPOSE 8000
+
+FROM base as prod
+#RUN mkdir /venv
+COPY backend/ ./
+#ADD backend/requirements.txt /venv/requirements.txt
+RUN python3 -m venv venv && \
+    venv/bin/pip3 install -r /venv/requirements.txt && \
+    venv/bin/pip3 install gunicorn==20.1.0 && \
+    /code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
+
+# todo later
+FROM ubuntu:20.04 as js
+RUN mkdir /code
+WORKDIR /code
+RUN apt-get install -y curl && \
+    curl -sL https://deb.nodesource.com/setup_14.x | bash - && \
+    apt-get install -y nodejs
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-docker build -t markerae .
+docker build -t markeraeprod --target prod .
+#docker build -t markerae .
 
 if [[ -z "$POSTGRES_HOST" ]]
 then
@@ -10,17 +11,19 @@ fi
 if [[ $1 == "manage" ]]
 then
   shift
-  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py "$@"
+  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markeraeprod /code/venv/bin/python3 /code/backend/manage.py "$@"
 elif [[ $1 == "deploy" ]]
 then
-  echo "env_variables:" > backend/appenv.yaml
-  echo "  POSTGRES_DB: ${POSTGRES_DB}" >> backend/appenv.yaml
-  echo "  POSTGRES_USER: ${POSTGRES_USER}" >> backend/appenv.yaml
-  echo "  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}" >> backend/appenv.yaml
-  echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
-  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
-  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py migrate
-  gcloud app deploy backend/app.yaml
+  echo foo
+  # todo build container, upload static files, push to cloud run with env variables
+#  echo "env_variables:" > backend/appenv.yaml
+#  echo "  POSTGRES_DB: ${POSTGRES_DB}" >> backend/appenv.yaml
+#  echo "  POSTGRES_USER: ${POSTGRES_USER}" >> backend/appenv.yaml
+#  echo "  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}" >> backend/appenv.yaml
+#  echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
+#  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
+#  docker run --mount type=bind,source="$(pwd)",target=/code --mount type=bind,source=/cloudsql,target=/cloudsql -e POSTGRES_DB="${POSTGRES_DB}" -e POSTGRES_USER="${POSTGRES_USER}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" -e POSTGRES_HOST="${POSTGRES_HOST}" -it markerae /code/venv/bin/python3 /code/backend/manage.py migrate
+#  gcloud app deploy backend/app.yaml
 else
   echo unknown command
 fi
