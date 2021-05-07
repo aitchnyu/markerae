b26efb24c2cd13e23783d1e3b1c8823a86df5dd9
Jesvin Jose <frank.einstien@gmail.com>
2021-05-07 18:27:15 +0530

    Include credentials and test deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-/code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
+echo "  POSTGRES_DB: ${POSTGRES_DB}" >> backend/appenv.yaml
+echo "  POSTGRES_USER: ${POSTGRES_USER}" >> backend/appenv.yaml
+echo "  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}" >> backend/appenv.yaml
+echo "  POSTGRES_HOST: ${POSTGRES_HOST}" >> backend/appenv.yaml
+#/code/venv/bin/python3 /code/backend/manage.py collectstatic --noinput
+docker build -t markerae .
+docker run --mount type=bind,source="$(pwd)",target=/code -it markerae /bin/bash
+docker run --mount type=bind,source="$(pwd)",target=/code -it markerae /code/venv/bin/python3 /code/backend/manage.py showmigrations
