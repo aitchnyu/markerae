5e97bf37576fb310faeba2c691123e1f14ac0dbf
Jesvin Jose <frank.einstien@gmail.com>
2021-05-08 19:46:49 +0530

    Ensuring sql proxy as a script again

---
+++ ensure_gc_sql_proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ ! -f ~/cloud_sql_proxy ]]; then
+  echo Downloading google cloud sql proxy
+  wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O ~/cloud_sql_proxy
+  chmod +x ~/cloud_sql_proxy
+fi
+sudo mkdir -p /cloudsql
+sudo chown $USER:$USER /cloudsql
+nohup ~/cloud_sql_proxy -instances="$PROJECT_ID:$REGION:$POSTGRES_INSTANCE" -dir=/cloudsql &
+PROXY_PID=$! # This is accessed as a global. This must be killed later.
+sleep 5 # Wait or psql may be unable to connect immediately
+echo $PROXY_PID
